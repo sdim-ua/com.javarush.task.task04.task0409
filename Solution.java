6a5d42e34f8da9fb15b34431669df0823a8284a8
sdim-ua <ingersol@yandex.ua>
2019-01-16 23:43:11 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,23 @@
+/* 
+Ближайшее к 10
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        displayClosestToTen(8, 11);
+        displayClosestToTen(7, 14);
+    }
+
+    public static void displayClosestToTen(int a, int b) {
+        // напишите тут ваш код
+
+    }
+
+    public static int abs(int a) {
+        if (a < 0) {
+            return -a;
+        } else {
+            return a;
+        }
+    }
+}
